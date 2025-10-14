b8b41445aa92b374ca21bbf40045d203b0855ffd
Jordan Williams <jordanw0412@gmail.com>
2025-10-14 00:00:56 -0400

    resolved prettier script

--- src/api/lib/types.ts
+++ src/api/lib/types.ts
@@ -1,14 +1,21 @@
 export interface Users {
-            id: string
-            name: string
-            email: string
-            passwordHash: string       
+  id: string;
+  name: string;
+  email: string;
+  passwordHash: string;
 }
 
 export interface Provider {
-    id: string; facilityName: string; doctorName: string; specialty: string; availableHours: string[];
+  id: string;
+  facilityName: string;
+  doctorName: string;
+  specialty: string;
+  availableHours: string[];
 }
 
 export interface Appointment {
-    id: string; providerId: string; dateTime: string; userEmail?: string; // Optional for anonymous bookings reservationCode: string;
+  id: string;
+  providerId: string;
+  dateTime: string;
+  userEmail?: string; // Optional for anonymous bookings reservationCode: string;
 }
