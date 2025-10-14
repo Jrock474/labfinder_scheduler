d0a9eaea818f42b8b6a254e6770cedef49fe46ee
Jordan Williams <jordanw0412@gmail.com>
2025-10-13 23:49:50 -0400

    initialized interfaces

---
+++ src/api/lib/types.ts
@@ -0,0 +1,14 @@
+export interface Users {
+            id: string
+            name: string
+            email: string
+            passwordHash: string       
+}
+
+export interface Provider {
+    id: string; facilityName: string; doctorName: string; specialty: string; availableHours: string[];
+}
+
+export interface Appointment {
+    id: string; providerId: string; dateTime: string; userEmail?: string; // Optional for anonymous bookings reservationCode: string;
+}
