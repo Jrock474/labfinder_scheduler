4b34eff2f3aa0bf33f0888f0bcfa95156a3111c4
Jordan Williams <jordanw0412@gmail.com>
2025-10-17 00:23:14 -0400

    moved api folder into the app folder

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { userDB } from "../../../lib/db";
+import { userDB } from "../../../../lib/db";
 
 export const GET = async (req: Request) => {
   await userDB.read();
