4a6aa3faa1b6ecddb17679e46774efc5cda42a1e
Jordan Williams <jordanw0412@gmail.com>
2025-10-17 00:10:58 -0400

    prettier

--- src/api/appointments/[id]/routes.ts
+++ src/api/appointments/[id]/routes.ts
@@ -1,19 +1,16 @@
-import { NextResponse } from 'next/server'
-import { appointmentDB } from '@/api/lib/db'
-import { Appointment } from '@/api/lib/types'
+import { NextResponse } from "next/server";
+import { appointmentDB } from "@/api/lib/db";
+import { Appointment } from "@/api/lib/types";
 
-export const GET = async (
-  req: Request,
-  params: { id: string } 
-) => {
-  const id = params.id
-  await appointmentDB.read()
+export const GET = async (req: Request, params: { id: string }) => {
+  const id = params.id;
+  await appointmentDB.read();
 
-  const provider = appointmentDB.data.find((p: Appointment) => p.id === id)
+  const provider = appointmentDB.data.find((p: Appointment) => p.id === id);
 
   if (!provider) {
-    return NextResponse.json({ error: 'Provider not found' }, { status: 404 })
+    return NextResponse.json({ error: "Provider not found" }, { status: 404 });
   }
 
-  return NextResponse.json(provider, { status: 200 })
-}
+  return NextResponse.json(provider, { status: 200 });
+};
--- src/api/providers/[id]/route.ts
+++ src/api/providers/[id]/route.ts
@@ -1,19 +1,16 @@
-import { NextResponse } from 'next/server'
-import { providerDB } from '@/api/lib/db'
-import { Provider } from '@/api/lib/types'
+import { NextResponse } from "next/server";
+import { providerDB } from "@/api/lib/db";
+import { Provider } from "@/api/lib/types";
 
-export const GET = async (
-  req: Request,
-  params: { id: string } 
-) => {
-  const id = params.id
-  await providerDB.read()
+export const GET = async (req: Request, params: { id: string }) => {
+  const id = params.id;
+  await providerDB.read();
 
-  const provider = providerDB.data.find((p: Provider) => p.id === id)
+  const provider = providerDB.data.find((p: Provider) => p.id === id);
 
   if (!provider) {
-    return NextResponse.json({ error: 'Provider not found' }, { status: 404 })
+    return NextResponse.json({ error: "Provider not found" }, { status: 404 });
   }
 
-  return NextResponse.json(provider, { status: 200 })
-}
+  return NextResponse.json(provider, { status: 200 });
+};
