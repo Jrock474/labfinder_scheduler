cfdd666facb916a8a78a6b88d0793ed3987a6541
Jordan Williams <jordanw0412@gmail.com>
2025-10-16 20:11:13 -0400

    added get route for users

---
+++ src/api/users/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+import { userDB } from "../lib/db";
+
+export const GET = async (req: Request) => {
+  await userDB.read();
+  const users = userDB.data;
+
+  NextResponse.json(users);
+};
